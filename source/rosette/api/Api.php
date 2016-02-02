c0ee269a82454b524b670c2fb031f73317bcd139
Chris Park <cpark@basistech.com>
2016-02-02 12:21:20 -0500

    Tiny change to trigger PR

--- source/rosette/api/Api.php
+++ source/rosette/api/Api.php
@@ -5,7 +5,7 @@
  *
  * Primary class for interfacing with the Rosette API
  *
- * @copyright 2014-2016 Basis Technology Corporation.
+ * @copyright 2015-2016 Basis Technology Corporation.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance
  * with the License. You may obtain a copy of the License at
