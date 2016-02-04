0e472ab5cf142a82d6cba5dfb42354fbbc34881e
fiona <fiona@basistech.com>
2016-02-04 14:30:04 -0500

    Improved error handling when invalid args are passed.

--- examples/docker/README.md
+++ examples/docker/README.md
@@ -2,11 +2,13 @@
 # Docker Image for PHP Examples
 ---
 ### Summary
-To simplify the running of the PHP examples, the Dockerfile will build an image and install the latest rosette-api library from composer.
+To simplify the running of the PHP examples, the Dockerfile will build an image and install the latest rosette-api library.
 
 ### Basic Usage
 Build the docker image, e.g. `docker build -t basistech/php:1.1 .`
 
-Run an example as `docker run -e API_KEY=api-key -v "path-to-example-source:/source" basistech/mono:1.1`
+Run an example as `docker run -e API_KEY=api-key -v "path-to-example-source:/source" basistech/php:1.1`
 
-To test against an alternate url, add `-e ALT_URL=alternate_url` before the `-v`
+To test against a specific source file, add `-e FILENAME=filename` before the `-v`
+
+Also, to test against an alternate url, add `-e ALT_URL=alternate_url` before the `-v`
--- examples/docker/run_php.sh
+++ examples/docker/run_php.sh
@@ -1,14 +1,54 @@
 #!/bin/bash
 
+#Gets called when the user doesn't provide any args
+function HELP {
+	echo -e "\nusage: source_file.php --key API_KEY [--url ALT_URL]"
+    echo "  API_KEY      - Rosette API key (required)"
+    echo "  FILENAME     - PHP source file (optional)"
+    echo "  ALT_URL      - Alternate service URL (optional)"
+    echo "Compiles and runs the source file(s) using the published rosette-api"
+    exit 1
+}
+
+#Gets API_KEY, FILENAME and ALT_URL if present
+while getopts ":API_KEY:FILENAME:ALT_URL" arg; do
+    case "${arg}" in
+        API_KEY)
+            API_KEY=${OPTARG}
+            usage
+            ;;
+        ALT_URL)
+            ALT_URL=${OPTARG}
+            usage
+            ;;
+        FILENAME)
+            FILENAME=${OPTARG}
+            usage
+            ;;
+    esac
+done
+
 # reference the API 
 curl "https://api.rosette.com/rest/v1/ping" -H "user_key: $1"
 
+#Copy the mounted content in /source to current WORKDIR
 cp /source/*.* .
 
 composer install
 
-if [ ! -z "$2" ]; then
-    find -maxdepth 1  -name '*.php' -print -exec php {} --key $1 --url $2 \;
-else
-	find -maxdepth 1  -name '*.php' -print -exec php {} --key $1 \;
+#Run the examples
+if [ ! -z ${API_KEY} ]; then
+	if [ ! -z ${FILENAME} ]; then
+		if [ ! -z ${ALT_URL} ]; then
+			php ${FILENAME} --key ${API_KEY} --url ${ALT_URL} 
+		else
+			php ${FILENAME} --key ${API_KEY} 
+   		fi
+	elif [ ! -z ${ALT_URL} ]; then
+    	find -maxdepth 1  -name '*.php' -print -exec php {} --key ${API_KEY} --url ${ALT_URL} \;
+	else
+		find -maxdepth 1  -name '*.php' -print -exec php {} --key ${API_KEY} \;
+    fi
+else 
+	HELP
 fi
