08a8cd8b9571784bab63de4fd4524639738be6bc
fiona <fiona@basistech.com>
2016-02-17 12:28:14 -0500

    Dockerfile now can run unit tests and generate gh-pages

--- docker/README.md
+++ docker/README.md
@@ -7,7 +7,7 @@ To simplify the running of the PHP examples, the Dockerfile will build an image 
 ### Basic Usage
 Build the docker image, e.g. `docker build -t basistech/php:1.1 .`
 
-Run an example as `docker run -e API_KEY=api-key -v "path-to-example-source:/source" basistech/php:1.1`
+Run an example as `docker run -e API_KEY=api-key -v "path-to-php-dir:/source" basistech/php:1.1`
 
 To test against a specific source file, add `-e FILENAME=filename` before the `-v`
 
--- docker/run_php.sh
+++ docker/run_php.sh
@@ -3,9 +3,11 @@
 #Gets called when the user doesn't provide any args
 function HELP {
     echo -e "\nusage: source_file.php --key API_KEY [--url ALT_URL]"
-    echo "  API_KEY      - Rosette API key (required)"
-    echo "  FILENAME     - PHP source file (optional)"
-    echo "  ALT_URL      - Alternate service URL (optional)"
+    echo "  API_KEY       - Rosette API key (required)"
+    echo "  FILENAME      - PHP source file (optional)"
+    echo "  ALT_URL       - Alternate service URL (optional)"
+    echo "  GIT_USERNAME  - Git username where you would like to push regenerated gh-pages (optional)"
+    echo "  VERSION       - Build version (optional)"
     echo "Compiles and runs the source file(s) using the local development source."
     exit 1
 }
@@ -25,6 +27,14 @@ while getopts ":API_KEY:FILENAME:ALT_URL" arg; do
             FILENAME=${OPTARG}
             usage
             ;;
+        GIT_USERNAME)
+            GIT_USERNAME=${OPTARG}
+            usage
+            ;;
+        VERSION)
+            VERSION={OPTARG}
+            usage
+            ;;
     esac
 done
 
@@ -37,9 +47,13 @@ function checkAPI {
     fi  
 }
 
+#Copy the mounted content in /source to current WORKDIR
+cp -r -n /source/* .
+
 #Run the examples
 if [ ! -z ${API_KEY} ]; then
     checkAPI
+    cd /php-dev/examples
 	if [ ! -z ${FILENAME} ]; then
 	    if [ ! -z ${ALT_URL} ]; then
 	        php ${FILENAME} --key ${API_KEY} --url=${ALT_URL} 
@@ -53,4 +67,24 @@ if [ ! -z ${API_KEY} ]; then
     fi
 else 
     HELP
+fi
+
+#Run unit tests
+cd /php-dev && ./vendor/bin/phpunit -v --bootstrap ./vendor/autoload.php ./tests/rosette/api/ApiTest.php
+
+#Generate gh-pages and push them to git account (if git username is provided)
+if [ ! -z ${GIT_USERNAME} ] && [ ! -z ${VERSION} ]; then
+    #clone php git repo to the root dir
+    cd /
+    git clone git@github.com:${GIT_USERNAME}/php.git
+    cd php
+    git checkout origin/gh-pages -b gh-pages
+    git branch -d develop
+    #generate gh-pages from development source and copy contents over to php repo
+    cd /php-dev
+    ./vendor/bin/phpdoc -t /php
+    cd /php
+    git add .
+    git commit -a -m "publish php apidocs ${VERSION}"
+    git push
 fi
