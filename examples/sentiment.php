d2cd338757106973517bd67973171bc46597adc1
fiona <fiona@basistech.com>
2016-03-11 12:50:15 -0500

    Sentiment example now shows doc-level and entity-level results

--- examples/sentiment.php
+++ examples/sentiment.php
@@ -16,6 +16,7 @@ if (!isset($options['key'])) {
 $sentiment_file_data = "<html><head><title>New Ghostbusters Film</title></head><body><p>Original Ghostbuster Dan Aykroyd, who also co-wrote the 1984 Ghostbusters film, couldn’t be more pleased with the new all-female Ghostbusters cast, telling The Hollywood Reporter, “The Aykroyd family is delighted by this inheritance of the Ghostbusters torch by these most magnificent women in comedy.”</p></body></html>";
 $api = isset($options['url']) ? new Api($options['key'], $options['url']) : new Api($options['key']);
 $params = new DocumentParameters();
+$params->set('language', 'eng');
 $content = $sentiment_file_data;
 $temp = tmpfile();  // write above html content to a temp file
 fwrite($temp, $content);
