7032acbb0bfd46256d3af285cd1fa58ae20dd92b
SamHausmann <hausmann.sam@gmail.com>
2016-06-08 10:51:51 -0400

    removed ./

--- docker/run_php.sh
+++ docker/run_php.sh
@@ -120,7 +120,7 @@ fi
 
 
 #Run unit tests
-cd /php-dev && ./bin/phpspec run
+cd /php-dev && ./bin/phpspec run --config=phpspec.yml --bootstrap=vendor/autoload.php --no-interaction --format=pretty
 
 #Run php-cs-fixer
 ./bin/php-cs-fixer fix . --dry-run --diff --level=psr2
