fd9756efc35e5c2a3668be24d797d9c3d4e0408d
Chris Park <cpark@basistech.com>
2017-12-21 09:55:37 -0500

    WS-1272 Multilingual example

---
+++ examples/language_multilingual.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Example code to call Rosette API to detect possible languages for a piece of text.
+ **/
+require_once dirname(__FILE__) . '/../vendor/autoload.php';
+use rosette\api\Api;
+use rosette\api\DocumentParameters;
+use rosette\api\RosetteException;
+
+$options = getopt(null, array('key:', 'url::'));
+if (!isset($options['key'])) {
+    echo 'Usage: php ' . __FILE__ . " --key <api_key> --url=<alternate_url>\n";
+    exit();
+}
+$language_multilingual_data = "TBD";
+$api = isset($options['url']) ? new Api($options['key'], $options['url']) : new Api($options['key']);
+$params = new DocumentParameters();
+$content = $language_multilingual_data;
+$params->set('content', $content);
+$api->setCustomHeader('X-RosetteAPI-App', 'php-app');
+$api->setOption('multilingual', 'true');
+
+try {
+    $result = $api->language($params);
+    var_dump($result);
+} catch (RosetteException $e) {
+    error_log($e);
+}
