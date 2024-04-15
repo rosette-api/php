7a33c79fe1d68923d6f2afbd4bfb93034ba84481
Adam Soos <asoos@basistech.com>
2024-04-15 12:52:55 +0200

    WS-3151: update versions to 1.29.0

--- source/rosette/api/Api.php
+++ source/rosette/api/Api.php
@@ -38,7 +38,7 @@ class Api
      *
      * @var string
      */
-    private static $binding_version = '1.26.0';
+    private static $binding_version = '1.29.0';
 
     /**
      * User key (required for Rosette API).
